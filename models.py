4e2accce3242e3c76d953a46aec1f36ab49dce25
Ian Neal <pyrocater@gmail.com>
2014-12-20 01:24:08 -0700

    Switch order of entry and tag, switch which belongs to which

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    def __unicode__(self):
-        return self.name
-
 class Entry(models.Model):
     pub_date = models.DateTimeField('date published')
     mod_date = models.DateTimeField('date modified')
-    tags = models.ManyToManyField(Tag)
     title = models.CharField(max_length=200)
     contents = models.TextField()
     def __unicode__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    entries = models.ManyToManyField(Entry)
+    def __unicode__(self):
+        return self.name
