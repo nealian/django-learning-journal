41eed0f21b495714bcf29bb25671092d62971d3f
Ian Neal <pyrocater@gmail.com>
2014-12-20 00:37:03 -0700

    add __unicode__() methods to models, as per tutorial

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class Entry(models.Model):
     pub_date = models.DateTimeField('date published')
@@ -9,3 +11,5 @@ class Entry(models.Model):
     tags = models.ManyToManyField(Tag)
     title = models.CharField(max_length=200)
     contents = models.TextField()
+    def __unicode__(self):
+        return self.title
