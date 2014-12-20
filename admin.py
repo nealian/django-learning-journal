cb01e5ab7b250f01b4ac6d0254d39ea097d7c9ae
Ian Neal <pyrocater@gmail.com>
2014-12-20 14:56:58 -0700

    Duh, admin.py needs to be in the Journal thing

---
+++ admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from journal.models import Entry, Tag
+
+class TagInline(admin.TabularInline):
+    model = Tag
+    extra = 0
+
+class EntryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Entry fields',     {'fields': ['title','contents']}),
+        ('Date information', {'fields': ['pub_date','mod_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [TagInline]
+
+admin.site.register(Entry, EntryAdmin)
+
