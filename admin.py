edca71444df246f6710ed58972f3ce6017c295f8
Ian Neal <pyrocater@gmail.com>
2014-12-20 17:10:41 -0700

    Oh, add .entries.through to model

--- admin.py
+++ admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from journal.models import Entry, Tag
 
 class TagInline(admin.TabularInline):
-    model = Tag
-#    extra = 1
+    model = Tag.entries.through
+    extra = 0
 
 class EntryAdmin(admin.ModelAdmin):
     fieldsets = [
