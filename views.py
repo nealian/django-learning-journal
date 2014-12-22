4947e19c7d5b87347588a5bfcefbb9b88960dc21
Ian Neal <pyrocater@gmail.com>
2014-12-22 12:46:36 -0500

    Add an index to views... mostly

--- views.py
+++ views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template import Context, loader
+from journal.models import Tag, Entry
+from django.http import HttpResponse
+
+def index(request):
+    latest_post_list = Entry.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('journal/index.html', {'latest_post_list': latest_post_list,})
