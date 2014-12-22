e035eb2122b00f10359ec1800b2679470c263997
Ian Neal <wopr@lanl.gov>
2014-12-22 11:11:30 -0700

    Add a couple views (missing the templates, but...)

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('journal.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<entry_id>\d+)/$', 'entry_detail'),
+    url(r'^tag/(?P<tag_id>\d+)/$', 'tag_detail'),
+    url(r'^tag/(?P<tag_id>\d+)/(?P<entry_id>\d+)/$', 'tag_entry_detail'),
+)
--- views.py
+++ views.py
@@ -1,7 +1,17 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 from journal.models import Tag, Entry
-from django.http import HttpResponse
+from django.http import Http404
 
 def index(request):
     latest_post_list = Entry.objects.all().order_by('-pub_date')[:5]
     return render_to_response('journal/index.html', {'latest_post_list': latest_post_list,})
+
+def entry_detail(request, entry_id):
+    entry = get_object_or_404(Entry, pk=entry_id)
+    return render_to_response('journal/entry_detail.html', {'entry': entry})
+
+def error_404(request):
+    return render_to_response('404.html', {'request': request})
+
+def error_500(request)
+    return render_to_response('500.html', {'request': request})
