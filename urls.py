2876bfa1244124b8ecda6863f638e9ce3aa019f0
Ian Neal <wopr@lanl.gov>
2014-12-22 13:15:07 -0700

    Add entry list template

--- urls.py
+++ urls.py
@@ -4,5 +4,4 @@ urlpatterns = patterns('journal.views',
     url(r'^$', 'index'),
     url(r'^(?P<entry_id>\d+)/$', 'entry_detail'),
     url(r'^tag/(?P<tag_id>\d+)/$', 'tag_detail'),
-    url(r'^tag/(?P<tag_id>\d+)/(?P<entry_id>\d+)/$', 'tag_entry_detail'),
 )
--- views.py
+++ views.py
@@ -3,9 +3,18 @@ from journal.models import Tag, Entry
 from django.http import Http404
 
 def index(request):
-    latest_post_list = Entry.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('journal/index.html', {'latest_post_list': latest_post_list,})
+    latest_entries = Entry.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('journal/entry_list.html', {'latest_entries': latest_entries,})
 
 def entry_detail(request, entry_id):
     entry = get_object_or_404(Entry, pk=entry_id)
-    return render_to_response('journal/entry_detail.html', {'entry': entry})
+    return render_to_response('journal/entry_detail.html', {'entry': entry,})
+
+def tag_detail(request, tag_id)
+    tag = get_object_or_404(Tag, pk=tag_id)
+    latest_entries_for_tag = tag.entries.all().order_by('-pub_date')[:25]
+    return render_to_response('journal/entry_list.html',
+    {
+        'tag': tag,
+        'latest_entries': latest_entries_for_tag,
+    })
