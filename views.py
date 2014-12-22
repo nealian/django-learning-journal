8663395b6f45effc3ca4476727bf358aedb5f86c
Ian Neal <wopr@lanl.gov>
2014-12-22 13:16:11 -0700

    Once again, forgot trailing : for def line

--- views.py
+++ views.py
@@ -10,7 +10,7 @@ def entry_detail(request, entry_id):
     entry = get_object_or_404(Entry, pk=entry_id)
     return render_to_response('journal/entry_detail.html', {'entry': entry,})
 
-def tag_detail(request, tag_id)
+def tag_detail(request, tag_id):
     tag = get_object_or_404(Tag, pk=tag_id)
     latest_entries_for_tag = tag.entries.all().order_by('-pub_date')[:25]
     return render_to_response('journal/entry_list.html',
