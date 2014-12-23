12edee70b0325cec58827e62911e35290576a8f8
Ian Neal <wopr@lanl.gov>
2014-12-22 17:17:45 -0700

    That code wasn't right for Django 1.4; fixed site_name stuff

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from journal.models import Tag, Entry
 from django.http import Http404
-from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.sites.models import Site
 
 def index(request):
-    current_site = get_current_site(request)
+    current_site = Site.objects.get_current()
     latest_entries = Entry.objects.all().order_by('-pub_date')[:5]
     return render_to_response('journal/entry_list.html',
     {
@@ -13,7 +13,7 @@ def index(request):
     })
 
 def entry_detail(request, entry_id):
-    current_site = get_current_site(request)
+    current_site = Site.objects.get_current()
     entry = get_object_or_404(Entry, pk=entry_id)
     return render_to_response('journal/entry_detail.html',
     {
@@ -22,7 +22,7 @@ def entry_detail(request, entry_id):
     })
 
 def tag_detail(request, tag_id):
-    current_site = get_current_site(request)
+    current_site = Site.objects.get_current()
     tag = get_object_or_404(Tag, pk=tag_id)
     latest_entries_for_tag = tag.entries.all().order_by('-pub_date')[:25]
     return render_to_response('journal/entry_list.html',
