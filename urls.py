39a2aa21cabe202c7e55f7d3088c9eb7d242d492
Ian Neal <wopr@lanl.gov>
2015-01-05 15:00:22 -0700

    Add url names to journal urls

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('journal.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<entry_id>\d+)/$', 'entry_detail'),
-    url(r'^tag/(?P<tag_id>\d+)/$', 'tag_detail'),
+    url(r'^$', 'index', name="journal-index"),
+    url(r'^(?P<entry_id>\d+)/$', 'entry_detail', name="journal-entry"),
+    url(r'^tag/(?P<tag_id>\d+)/$', 'tag_detail', name="journal-tag"),
 )
