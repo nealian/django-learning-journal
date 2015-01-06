a99a2bd65d58f81a29881102439fc35ca87d4c13
Ian Neal <wopr@lanl.gov>
2015-01-06 14:05:24 -0700

    Kinda flesh out the public views, but without actual auth; just default to 404 on not public

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from journal.models import Tag, Entry
 from django.http import Http404
 from django.contrib.sites.models import Site
@@ -19,34 +19,35 @@ test_sidebar_contents = [
     },
 ]
 
+common_params = {
+    'site_name': Site.objects.get_current().name,
+    'sidebar_contents': test_sidebar_contents,
+}
+
 def index(request):
-    current_site = Site.objects.get_current()
     latest_entries = Entry.objects.filter(public=True).order_by('-pub_date')[:5]
-    return render_to_response('journal/entry_list.html',
-    {
-        'site_name': current_site.name,
-        'latest_entries': latest_entries,
-        'sidebar_contents': test_sidebar_contents,
-    })
+    return render(response, 'journal/entry_list.html', 
+                  dict(common_params.items() + {
+                      'latest_entries': latest_entries,
+                  }.items()))
 
 def entry_detail(request, entry_id):
-    current_site = Site.objects.get_current()
-    entry = get_object_or_404(Entry, pk=entry_id)
-    return render_to_response('journal/entry_detail.html',
-    {
-        'site_name': current_site.name,
-        'entry': entry,
-        'sidebar_contents': test_sidebar_contents,
-    })
+    try:
+        entry = Entry.objects.get(pk=endry_id)
+        if not entry.public:
+            raise Http404
+    except Entry.DoesNotExist:
+        raise Http404
+    return render(response, 'journal/entry_detail.html',
+                  dict(common_params.items() + {
+                      'entry': entry,
+                  }.items()))
 
 def tag_detail(request, tag_id):
-    current_site = Site.objects.get_current()
     tag = get_object_or_404(Tag, pk=tag_id)
     latest_entries_for_tag = tag.entries.filter(public=True).order_by('-pub_date')[:25]
-    return render_to_response('journal/entry_list.html',
-    {
-        'site_name': current_site.name,
-        'tag': tag,
-        'latest_entries': latest_entries_for_tag,
-        'sidebar_contents': test_sidebar_contents,
-    })
+    return render(response, 'journal/entry_list.html',
+                  dict(common_params.items() + {
+                      'tag': tag,
+                      'latest_entries': latest_entries_for_tag,
+                  }.items()))
